0d6e3fab2284dc6dd6dfcab769234d7a8fde25c6
Tolga Tosun <tlgtsn08@gmail.com>
2025-04-20 17:43:19 +0300

    next.config.ts dosyasındaki deneysel turbo yapılandırması ve önbellek ayarları kaldırıldı, trailingSlash özelliği devre dışı bırakıldı.

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true
-  },
-  trailingSlash: true,
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        // Cache yapılandırması
-        cache: true
-      }
-    }
-  },
-  /* config options here */
+  }
 };
 
 export default nextConfig;
