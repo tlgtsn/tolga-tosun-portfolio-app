fae2588d83c8c9cd98c5cf4369c950c396d94252
Tolga Tosun <tlgtsn08@gmail.com>
2025-04-20 23:46:41 +0300

    terraform/main.tf dosyasına AWS S3 bucket politikası eklendi, CloudFront erişimi için gerekli izinler tanımlandı.

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.site.arn}/*"
+      }
+    ]
+  })
+} 
