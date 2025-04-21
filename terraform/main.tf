762c27bb2fe7d431527e07669493cd2ae670175d
Tolga Tosun <tlgtsn08@gmail.com>
2025-04-22 01:24:28 +0300

    S3 bucket politikası güncellendi; erişim izinleri genel hale getirildi. CloudFront dağıtımı için S3 web sitesi yapılandırması eklendi ve kök nesne ayarları güncellendi.

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,20 +22,17 @@ resource "aws_cloudfront_origin_access_identity" "oai" {
 }
 
 # S3 Bucket Policy
-resource "aws_s3_bucket_policy" "policy" {
+resource "aws_s3_bucket_policy" "site" {
   bucket = aws_s3_bucket.site.id
-
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "AllowCloudFrontAccess"
+        Sid       = "PublicReadGetObject"
         Effect    = "Allow"
-        Principal = {
-          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
-        }
-        Action   = "s3:GetObject"
-        Resource = "${aws_s3_bucket.site.arn}/*"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.site.arn}/*"
       }
     ]
   })
@@ -51,30 +48,45 @@ resource "aws_s3_bucket_public_access_block" "site" {
   restrict_public_buckets = true
 }
 
-# CloudFront Distribution
-resource "aws_cloudfront_distribution" "dist" {
-  enabled             = true
-  is_ipv6_enabled    = true
-  comment            = "CF for tolgatosun.dev"
-  default_root_object = "index.html"
-  price_class        = "PriceClass_All"
+# S3 Bucket Website Configuration
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
 
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "site" {
   origin {
-    domain_name = aws_s3_bucket.site.bucket_regional_domain_name
+    domain_name = aws_s3_bucket_website_configuration.site.website_endpoint
     origin_id   = "S3-${aws_s3_bucket.site.id}"
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
     }
   }
 
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3-${aws_s3_bucket.site.id}"
 
     forwarded_values {
       query_string = false
+
       cookies {
         forward = "none"
       }
@@ -93,10 +105,6 @@ resource "aws_cloudfront_distribution" "dist" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:152914900895:certificate/422ab345-cc09-47ee-add0-f41d87f010cc"
-    ssl_support_method  = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    cloudfront_default_certificate = true
   }
-
-  aliases = ["tolgatosun.dev"]
 } 
